1e83f05371abdac4f76776441ba5293464672535
Alicia Wilkerson <aliciawilkerson@babers.com>
2019-10-25 12:19:50 -0500

    starting game data change

--- src/js/game/npc/0000.js
+++ src/js/game/npc/0000.js
@@ -1,12 +1,9 @@
----
-name: Phoenix "Nix"
-shortname: nix
-always: 
-    blackbox: 0001
-items: []
----
-
-Your name is Phoenix, but you prefer to be called **Nix**. You are unlike most people you know.  One of a kind.  But Earth is a large place and Old New York is such a small one in comparison, but you are the only mutant like yourself.  You are an elementvoyant.  Which basically means you see the world differently than the rest of the world.  You see the world in terms of the elements they are made up of.  Your world is a mass of colored lights that if you had actual eyes to see would drive you insane, and has driven many elementvoyants to the brink and past in most cases.  You were lucky to be born with no eyes and thus do not have the conflicting brain signals to deal with.  Your mutations perfectly matched your abilities unlike most others.
+export default {
+    name: 'Phoenix "Nix"',
+    short_name: 'nix',
+    always: ['blackbox'], // items always has on self
+    items: [], // items current on self
+    description: `Your name is Phoenix, but you prefer to be called **Nix**. You are unlike most people you know.  One of a kind.  But Earth is a large place and Old New York is such a small one in comparison, but you are the only mutant like yourself.  You are an elementvoyant.  Which basically means you see the world differently than the rest of the world.  You see the world in terms of the elements they are made up of.  Your world is a mass of colored lights that if you had actual eyes to see would drive you insane, and has driven many elementvoyants to the brink and past in most cases.  You were lucky to be born with no eyes and thus do not have the conflicting brain signals to deal with.  Your mutations perfectly matched your abilities unlike most others.
 
 In order to hide your eyeless features you tend to wear a pair of expensive black sunglasses.  You rarely remove them.  Your lack of eyes prohibits many usual activities such as reading or watching the terminals.  There is very little information in the elements to distinguish the minimal differences of light waves reflecting off of a surface in text or on screen.
 
@@ -19,3 +16,5 @@ Other's have described you as having chestnut brown hair that you keep short on 
 You are taller than most people you've met.  And your frame is that of someone who does a lot of walking and running from place to place.  You wear all black.  Your pants are made of durable linen that is almost bullet proof but is definitely slash and thrust proof.  Your shirt is made of the same material.  You almost always wear a black leather trench coat that falls almost to your ankles.  On your feet are a pair of black combat boots meant for heavy walking and protection. 
 
 Your final accessory is the tiny **blackbox** that houses MOUSE's portable AI system that is integrated with your cybernetic implants.  Above your right eye you purchased the best quality cybernetic prosthetic camera and system to aid in translating the real world to the world you see.  The camera system is connected to the implanted bone speaker and microphone in order for you to whisper commands to MOUSE with little volume and hear what he has to report back from the camera and speaker implants.  Both systems are wirelessly connected to MOUSE through a short range wavelength only known to this particular AI.
+`,
+};
---
+++ src/js/game/npc/0001.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'MOUSE',
+    short_name: 'mouse',
+    always: ['blackbox'], // items always has on self
+    items: ['blackbox'], // items current on self
+    description:`MOUSE is your AI.  He is a tiny black box that when not on your coffee table at home is in the left interior breast coat pocket of your black leather trench coat, or your front right pocket if you take the coat off for some unfathomable reason.
+    `,
+}
